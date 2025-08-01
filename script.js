7e9d9c492c94de39c63dcac6d3a94a120f21abd5
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-08-02 01:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+ 
+  if (s.length === 0) return "";
+ 
+  const spaceIndex = s.indexOf(" ");
+
+
+  if (spaceIndex === -1) return s;
+
+
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
